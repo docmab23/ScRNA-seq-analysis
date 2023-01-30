320644a1a14f52e3d1af36a8751fda938da1cac9
Mannan Bhola <31487695+docmab23@users.noreply.github.com>
2023-01-29 22:27:39 -0500

    Delete kk.sh

--- kk.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch --env-filter \
-	    'if [ $GIT_COMMIT = 119f9ecf58069b265ab22f1f97d2b648faf932e0 ]
-     then
-	              export GIT_AUTHOR_DATE="Wed March 16 21:38:53 2022 -0800"
-		               export GIT_COMMITTER_DATE="Wed March 16  01:01:01 2022 -0700"
-			            fi'
